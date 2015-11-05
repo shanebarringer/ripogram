a1da7b52e22225831f72eeeb80f8956e262bdf75
Shane Barringer <shane.barringer@outlook.com>
2015-11-05 10:43:06 -0500

    adds Posts model with caption attribute / adds @post instance variable to 'new' action in posts controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151105153943) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
